d70c9a3d454baf801472b0541aefbdf73e932d22
IvanOhlopkov <iokhlopkov@gmail.com>
2017-12-17 21:25:58 +0300

    Lesson2Task1(v.2)

--- Lesson1/Task1.java
+++ Lesson1/Task1.java
@@ -1,35 +1,25 @@
+package Lesson1;
 import java.util.Scanner;
 /**
- * класс calc позволяет сложить два числа. сумма выводится с 4-мя знаками после запятой
+ * класс Task1 позволяет сложить два числа. сумма выводится с 4-мя знаками после запятой
  * @author Ivan Ohlopkov
- * */
-public class calc {
-    public static void calculate(){
+ */
+public class Task1 {
+    public static void main(String[] args) {
          /*создаем объект класса Scanner,
         объявляем переменные а,b,sum с типом double и присваиваем метод nextDouble для считывания ввода
          */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число: ");
         double a = scanner.nextDouble();
-        System.out.println("Введите операцию: +,-,/,*");
-        String op = scanner.next();
         System.out.println("Введите второе число: ");
         double b = scanner.nextDouble();
-        double sum = 0;
+        double sum;
         scanner.close();
-        if(op.equals("+") )
             sum = a + b;
-        else if(op.equals("-"))
-            sum = a - b;
-        else if (op.equals("/"))
-            sum = a / b;
-        else if (op.equals("*"))
-            sum = a * b;
-        else
-            System.out.println("Вы ввели не правильную операцию!");
         //используем метод Math.round - округляем sum умноженной на 10000 до целого, и делим на 10000
         sum = Math.round(sum * 10000.0) / 10000.0;
         //выводим сумму сложения в формате %.4f, чтобы вывод был в 4 знака после запятой
-        System.out.printf("Результат операции равен %.4f: ", sum);
+        System.out.printf("Результат сложения равен %.4f: ", sum);
     }
 }
