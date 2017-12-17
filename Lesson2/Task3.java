43e7c737eef10b2aeefca94ab5d38eecc5f53372
IvanOhlopkov <iokhlopkov@gmail.com>
2017-12-17 21:34:17 +0300

    Lesson2Task3(v.2)

--- Lesson2/Task3.java
+++ Lesson2/Task3.java
@@ -1,19 +1,20 @@
+package Lesson2;
 import java.util.Scanner;
 /**
- * класс base позволяет вызвать на выбор калькулятор или ввод массива из слов
+ * класс Task3 позволяет вызвать на выбор калькулятор или ввод массива из слов
  * @author Ivan Ohlopkov
  * */
-public class
-base {
+public class Task3 {
     public static void main(String[] args){
         Scanner choice = new Scanner(System.in);
         System.out.println("Выберите программу для выполнения. 1- калькулятор, 2-поиск максимального слова в массиве");
         byte a = choice.nextByte();
         if(a == 1)
-            calc.calculate();
+            Task1.calculate();
         else if (a == 2)
-            massiv.intext();
+            Task2.intext();
         else
             System.out.println("Вы ввели не верное значение выбора");
+        choice.close();
     }
 }
