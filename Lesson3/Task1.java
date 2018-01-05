b203a150785165ecc4e1b53834b5d96266a70ea5
IvanOhlopkov <iokhlopkov@gmail.com>
2018-01-06 00:22:46 +0300

    Lesson3Task1(v.4)

--- Lesson3/Task1.java
+++ Lesson3/Task1.java
@@ -3,7 +3,7 @@ package Lesson3;
  * класс Task1 позволяет провести операцию для двух чисел. сумма выводится с 4-мя знаками после запятой
  * вызывает метод из класса Task1Calculation, предварительно объявленного в конструкторе
  * @author Ivan Ohlopkov
- * */
+ */
 public class Task1{
     public static void main( String[] args ){
         Task1Calculation obj = new Task1Calculation();
